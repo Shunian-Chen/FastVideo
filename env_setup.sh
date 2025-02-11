6af6028d62e264b80f0e4f5a11db18d0d77b5dd8
Shunian-Chen <1418314920@qq.com>
2025-02-11 17:28:15 +0800

    setup update

--- env_setup.sh
+++ env_setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # install torch
-pip install torch==2.5.0 torchvision --index-url https://download.pytorch.org/whl/cu121
+pip install torch==2.5.0 torchvision --index-url https://download.pytorch.org/whl/cu121 -i https://pypi.tuna.tsinghua.edu.cn/simple
 
 # install FA2 and diffusers
-pip install packaging ninja && pip install flash-attn==2.7.0.post2 --no-build-isolation 
+pip install packaging ninja && pip install flash-attn==2.7.0.post2 --no-build-isolation -i https://pypi.tuna.tsinghua.edu.cn/simple
 
-pip install -r requirements-lint.txt
+pip install -r requirements-lint.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 
 # install fastvideo
-pip install -e .
+pip install -e . -i https://pypi.tuna.tsinghua.edu.cn/simple
